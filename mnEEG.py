16654338480a48aec643a53d8637b778144f1199
srosorio <sosoriogaleano@mgh.harvard.edu>
2023-07-14 14:45:26 -0400

    FEATURE: added few lines to template scripts

--- mnEEG.py
+++ mnEEG.py
@@ -7,8 +7,10 @@ class mnEEG():
     """
 
     def __init__(self, subject_id):
-        self.id         = subject_id
-        self.rawfiles   =  [] #function to get subject paths#
+        self.id          = subject_id
+        self.raw_files   =  [] #function to get subject paths#
+        self.ica_files   =  []
+        self.epoch_files =  [] #function to get subject paths#
 
 
     def forward_modeling():
@@ -19,7 +21,7 @@ class mnEEG():
 
         for file in self.epoch_files:
             epochs = mne.read_epochs(file)
-            
+
     def source_localization():
         """
         Computes forward solution
