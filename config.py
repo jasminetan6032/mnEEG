5d08f46482b2cef712a9365a126d37844fac3727
srosorio <sosoriogaleano@mgh.harvard.edu>
2023-07-14 16:06:09 -0400

    FEATURE: Add automatic user detection

--- config.py
+++ config.py
@@ -1,2 +1,8 @@
+from os import getlogin
 
-subjects_dir = 'D:/source_hackaton/mneEEG_subject/'
+users_dictionary = {'jwt30' :'/local_mount/space/hypatia/2/users/Jasmine/mnEEG_subjects',
+                    'sao42' : '/local_mount/space/tapputi/1/users/sergio/mnEEG_subjects' ,
+                    'asus'  : '/D:/source_hackaton/mneEEG_subject/'}
+
+subjects_dir = users_dictionary[getlogin()]
+
