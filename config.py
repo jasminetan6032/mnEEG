d2fb612921742cdb567cf57fb85358eb5b64c1bf
srosorio <sosoriogaleano@mgh.harvard.edu>
2023-07-14 17:14:56 -0400

    EDIT

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from os import getlogin
 
 users_dictionary = {'jwt30' :'/local_mount/space/hypatia/2/users/Jasmine/mnEEG_subjects',
                     'sao42' : '/local_mount/space/tapputi/1/users/sergio/mnEEG_subjects' ,
-                    'asus'  : '/D:/source_hackaton/mneEEG_subject/'}
+                    'asus'  : "D:\\source_hackaton\\mneEEG_subjects\\"}
 
 subjects_dir = users_dictionary[getlogin()]
 
